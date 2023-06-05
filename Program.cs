a508d9e544fe6ef879b0ef79a9a9fe812d5101f0
Mujeebanwar1 <mujeebanwar76@gmail.com>
2023-06-06 01:15:17 +0530

    createrd ladder

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using SankeAndLadder;
+using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To SNAKE AND LADDER Game");
+            SnakeLadder playerOne = new SnakeLadder();
+            playerOne.DieRolling();
+            playerOne.PlayerCheck();
+        }
+    }
+}
